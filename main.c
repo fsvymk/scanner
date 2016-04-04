d15bbe7fce778a242351ffdc7bfc2db9648fadd2
Mihail <fsvymk@gmail.com>
2016-04-04 16:13:08 +0300

    10 times

--- main.c
+++ main.c
@@ -18,29 +18,24 @@ int main() {
     }
 
     printf("Scanner. Select Parameter:\n");
-    printf("1. VOLD:DC");
-    //char *command  = "VOLT:DC:RANG 7\n";
-    char *command[50];
-    //char *command = "VOLT:DC:RANG?\n";
+    printf("READ? 10 times.\n");
 
-    while(1==1){
-        // Цикл
+    char *command[50];
+    char cmd[] = "READ?";
+
+    int R = 10; // R - Range
+    int szStr = strlen(command);
+
+    for (i=0; i<R; i++){
+
         int fd = open("/dev/usbtmc0", O_RDWR);
-        gets(command);
-        int szStr = strlen(command);
-        //printf("Length is %d\n", szStr);
-        write(fd, command,  szStr);
+
+
+        write(fd, cmd,  szStr);
         write(fd, '\n', 1);
 
         ssize_t size = read(fd, &buf, 32);
-
-        char x = buf[0];
-        char y = buf[1];
-
-        //printf("Read byte %d %d | %c %c\n", x, y , buf[0], buf[1]);
-       // printf("Size %d\n", size);
-        printf("==> %s \n\n", buf);
-
+        printf("> %s \n", buf);
 
         close(fd);
     }
